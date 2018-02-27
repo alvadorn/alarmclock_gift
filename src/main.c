79ccc441c241b6d5381929cc6a6fcc975721d0cd
Igor Sant'ana <igorba2005@gmail.com>
2018-02-27 11:48:37 -0300

    Initial configuration

--- src/main.c
+++ src/main.c
@@ -1,10 +1,26 @@
+#include "defs.h"
+#include "settings.h"
+#include "spi.h"
+
+static inline void config() {
+    configClock();
+
+    configPins();
+
+    configInterruptions();
+
+    initializeSPI();
+}
 
 
 static inline void loop() {
 
 }
 
+
 int main() {
+
+    config();
 
     while(1) {
         loop();
