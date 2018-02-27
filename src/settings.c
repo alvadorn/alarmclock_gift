e1fbaa44feb03d9b521aa14adda2add95d6030ef
Igor Sant'ana <igorba2005@gmail.com>
2018-02-27 11:48:09 -0300

    Initial settings functions

---
+++ src/settings.c
@@ -0,0 +1,36 @@
+#include "settings.h"
+
+
+inline void configClock() {
+    OSCCON = 0b01100010;
+}
+
+
+inline void configVoltage() {
+    
+}
+
+inline void configPins() {
+    // RA4 and RA5 as output
+    TRISA &= ~(3 << 4);
+
+    // RB4 and RB6 as output
+    TRISB &= 0xAF;
+
+    /**
+     * RC0 -> BTN Down IN
+     * RC1 -> BTN Left IN
+     * RC2 -> BTN Center IN
+     * RC3 -> SCLK OUT
+     * RC4 -> DC OUT
+     * RC5 -> SDIN OUT
+     * RC6 -> LED Control OUT
+     * RC7 -> DHT in/out
+    */
+    TRISC = 0x07;
+       
+}
+
+inline void configInterruptions() {
+    
+}
---
+++ src/settings.h
@@ -0,0 +1,11 @@
+#ifndef __SETTINGS_H__
+#define __SETTINGS_H__
+
+#include "defs.h"
+
+extern inline void configClock();
+extern inline void configVoltage();
+extern inline void configPins();
+extern inline void configInterruptions();
+
+#endif 
